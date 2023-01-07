12b078cc89a9a217f05dab24ac5847d126050ac9
Dfunn1k <danychavezdev@gmail.com>
2023-01-07 13:26:30 -0500

    ci: added dependencies

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
-apt-get install -y make wget markdown zip
+sudo apt-get install -y make wget zip
+npm install markdownlint-cli -g -y
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
 tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
 mv hugo /usr/local/bin/hugo
