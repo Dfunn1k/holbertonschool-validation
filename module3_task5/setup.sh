70eaab99836ae153bbe8443e3fdfb6b616cb27a8
Dfunn1k <danychavezdev@gmail.com>
2023-01-09 22:29:50 -0500

    update module5

---
+++ module3_task5/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update
+sudo apt-get install -y make wget zip
+sudo npm install -g markdownlint-cli
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+mv hugo /usr/local/bin/hugo
+make build
